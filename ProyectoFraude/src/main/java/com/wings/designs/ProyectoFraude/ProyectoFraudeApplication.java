39b5fc8969aa7261f04f1284de8ded4f4a3cab71
Sebastian Zapata <sebastian.zapata@alumnos.ucn.cl>
2021-05-14 19:10:43 -0400

    Spring con sus dependencias

---
+++ ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/ProyectoFraudeApplication.java
@@ -0,0 +1,13 @@
+package com.wings.designs.ProyectoFraude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFraudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFraudeApplication.class, args);
+	}
+
+}
