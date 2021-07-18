b232e3623ca855dc2e30dc0432f5e16b862364e1
Nicolas Henriquez <nicolashex1@gmail.com>
2021-07-18 03:14:25 -0400

    remove unused imports on client controller class

--- ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/controller/RegistrationController.java
+++ ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/controller/RegistrationController.java
@@ -7,15 +7,10 @@ package com.wings.designs.ProyectoFraude.controller;
 import com.wings.designs.ProyectoFraude.requestbody.RegistrationRequest;
 import com.wings.designs.ProyectoFraude.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 
 @RestController
