bb7e3fc4d892048670c606861e51d18e012f25aa
Nicolas Henriquez <nicolashex1@gmail.com>
2021-07-18 03:13:26 -0400

    add documentation to client controller class

--- ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/controller/ClientController.java
+++ ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/controller/ClientController.java
@@ -9,15 +9,38 @@ import com.wings.designs.ProyectoFraude.service.ClientService;
 import com.wings.designs.ProyectoFraude.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+/**
+ * The rest Controller in charge of the {@link Client client}
+ * resource's endpoints on the API.
+ * @author Nicolas Henriquez
+ * @author Sebastian Zapata
+ * @author Ignacio Cabrera
+ * @author Maikol Leiva
+ * @version 1.0
+ */
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
+    /**
+     * service that allow to take data from
+     * the ticket resource.
+     */
     private final TicketService ticketService;
+    /**
+     * service that allow to take data from
+     * the client resource.
+     */
     private final ClientService clientService;
 
+    /**
+     * Main constructor.
+     * @param ticketService service that allow to take data from
+     *                      the ticket resource. It's autowired.
+     * @param clientService service that allow to take data from
+     *                      the client resource. It's autowired.
+     */
     @Autowired
     public ClientController(final TicketService ticketService,
                             final ClientService clientService) {
@@ -25,6 +48,11 @@ public class ClientController {
         this.clientService = clientService;
     }
 
+    /**
+     * Get all the clients from the database.
+     * @return A list of clients, if there's clients on the database.
+     * Or an empty list in case there's no clients.
+     */
     @GetMapping
     public List<Client> getClients() {
         return clientService.getClients();
