8e22efb2396e965b5415ac5aaa8b5cb09050ee0f
Nicolas Henriquez <nicolashex1@gmail.com>
2021-07-18 16:55:13 -0400

    add documentation to JwtSecretKey class

--- ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/jwt/JwtSecretKey.java
+++ ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/jwt/JwtSecretKey.java
@@ -12,18 +12,33 @@ import org.springframework.context.annotation.Configuration;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Class that manages the secret key of the JWT.
+ */
 @Configuration
 public class JwtSecretKey {
 
+    /**
+     * Provides the secret key given on the application.properties file.
+     */
     private final JwtConfig jwtConfig;
 
+    /**
+     * Main constructor. Autowired.
+     * @param jwtConfig the configuration class that has the raw secret key.
+     */
     @Autowired
-    public JwtSecretKey(JwtConfig jwtConfig) {
+    public JwtSecretKey(final JwtConfig jwtConfig) {
         this.jwtConfig = jwtConfig;
     }
 
+    /**
+     * Returns the secret key.
+     * @return the secret key.
+     */
     @Bean
     public SecretKey secretKey() {
-        return Keys.hmacShaKeyFor(jwtConfig.getSecretKey().getBytes(StandardCharsets.UTF_8));
+        return Keys.hmacShaKeyFor(
+                jwtConfig.getSecretKey().getBytes(StandardCharsets.UTF_8));
     }
 }
