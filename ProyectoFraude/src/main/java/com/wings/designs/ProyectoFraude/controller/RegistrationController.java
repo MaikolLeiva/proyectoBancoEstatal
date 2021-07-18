203f1d27bfd1961fbed14ac8bb20037d6b0bf33a
Nicolas Henriquez <nicolashex1@gmail.com>
2021-07-18 03:23:32 -0400

    add documentation to registration controller

--- ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/controller/RegistrationController.java
+++ ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/controller/RegistrationController.java
@@ -13,18 +13,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
+/**
+ * The rest Controller in charge of the Registration process on the API.
+ * @author Nicolas Henriquez
+ * @author Sebastian Zapata
+ * @author Ignacio Cabrera
+ * @author Maikol Leiva
+ * @version 1.0
+ */
 @RestController
 @RequestMapping("/register")
 public class RegistrationController {
+    /**
+     * service that allow to take data from
+     * the client resource.
+     */
     final ClientService clientService;
 
+    /**
+     * Main constructor.
+     * @param clientService service that allow to take data from
+     *                      the client resource. It's autowired.
+     */
     @Autowired
     public RegistrationController(ClientService clientService) {
         this.clientService = clientService;
     }
 
+    /**
+     * Register a new client on the API, with the data given on
+     * the post Request.
+     * @param registrationRequest It's a {@link RegistrationRequest RegistrationRequest}
+     *                            object that posses all the attributes necessaries
+     *                            for a registration.
+     */
     @PostMapping
-    public void registerNewUser(@Valid @RequestBody RegistrationRequest registrationRequest) {
+    public void registerNewUser(@Valid @RequestBody
+                                    final RegistrationRequest registrationRequest) {
         clientService.addNewClient(registrationRequest);
 
     }
