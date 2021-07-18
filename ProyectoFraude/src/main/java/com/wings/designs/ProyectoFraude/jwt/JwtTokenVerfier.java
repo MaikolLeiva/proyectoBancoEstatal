0c7301c3d01c4773faa13268408fe81700f18020
Nicolas Henriquez <nicolashex1@gmail.com>
2021-07-18 17:30:12 -0400

    add documentation to JwtTokenVerifier class

--- ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/jwt/JwtTokenVerfier.java
+++ ProyectoFraude/src/main/java/com/wings/designs/ProyectoFraude/jwt/JwtTokenVerfier.java
@@ -25,42 +25,87 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Class that allow to verify the token given by the client.
+ */
 public class JwtTokenVerfier extends OncePerRequestFilter {
 
+    /**
+     * The secret key used to validate the token.
+     */
     private final SecretKey secretKey;
+
+    /**
+     * Configuration class that provides with information like
+     * the expiration time  of the token or the prefix.
+     */
     private final JwtConfig jwtConfig;
 
-    public JwtTokenVerfier(SecretKey secretKey, JwtConfig jwtConfig) {
+    /**
+     * Main constructor.
+     *
+     * @param secretKey the secret key used on the token.
+     * @param jwtConfig the configuration class with
+     *                  information about the token.
+     */
+    public JwtTokenVerfier(final SecretKey secretKey,
+                           final JwtConfig jwtConfig) {
         this.secretKey = secretKey;
         this.jwtConfig = jwtConfig;
     }
 
+    /**
+     * Simple implementation  of doFilterInternal that filters request
+     * to check if there's any errors adding a layer to authenticate
+     * if there's issues on the token provided.
+     *
+     * @param request     The request to process
+     * @param response    The response associated with the request
+     * @param filterChain Provides access to the next filter in the
+     *                    chain for this filter to pass the request
+     *                    and response to for further processing.
+     * @throws IOException      if there's an error on the I/O on the request.
+     * @throws ServletException if the processing on the request fails
+     *                          for any other reason.
+     */
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(final HttpServletRequest request,
+                                    final HttpServletResponse response,
+                                    final FilterChain filterChain)
             throws ServletException, IOException {
-        String authorizationHeader = request.getHeader(jwtConfig.getAuthorizationHeader());
-
-        if (authorizationHeader == null || authorizationHeader.isEmpty() ||
-                !authorizationHeader.startsWith(jwtConfig.getTokenPrefix())) {
+        String authorizationHeader = request.getHeader(
+                jwtConfig.getAuthorizationHeader());
+        if (authorizationHeader == null || authorizationHeader.isEmpty()
+                || !authorizationHeader
+                .startsWith(jwtConfig.getTokenPrefix())) {
             filterChain.doFilter(request, response);
             return;
         }
-        String token = authorizationHeader.replace(jwtConfig.getTokenPrefix(), "");
+        String token = authorizationHeader.replace(
+                jwtConfig.getTokenPrefix(), "");
         try {
 
-            Jws<Claims> claimsJws = Jwts.parserBuilder().setSigningKey(secretKey)
+            Jws<Claims> claimsJws = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
                     .build().parseClaimsJws(token);
             Claims body = claimsJws.getBody();
             String username = body.getSubject();
-            List<Map<String, String>> authorities = (List<Map<String, String>>) body.get("authorities");
+            List<Map<String, String>> authorities =
+                    (List<Map<String, String>>) body.get("authorities");
             Set<SimpleGrantedAuthority> authority = authorities.stream()
-                    .map(m -> new SimpleGrantedAuthority(m.get("authority"))).collect(Collectors.toSet());
-            Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authority);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                    .map(m -> new SimpleGrantedAuthority(m.get("authority")))
+                    .collect(Collectors.toSet());
+            Authentication authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            username, null, authority);
+            SecurityContextHolder
+                    .getContext()
+                    .setAuthentication(authentication);
 
 
         } catch (JwtException e) {
-            throw new IllegalStateException(String.format("Token %s cannot be trusted", token));
+            throw new IllegalStateException(
+                    String.format("Token %s cannot be trusted", token));
         }
         filterChain.doFilter(request, response);
     }
